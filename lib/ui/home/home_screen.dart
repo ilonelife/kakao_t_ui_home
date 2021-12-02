88575b4fc740959c0758fdcbde2f0e6b40969368
iloneslife <jasony1@naver.com>
2021-12-02 17:43:40 +0900

    버튼 메뉴를 함수로 분리 함

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kakao_t_ui_exam/ui/home/widgets/ad.dart';
+import 'package:kakao_t_ui_exam/ui/home/widgets/menu_button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -27,131 +28,57 @@ class HomeScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Column(
-                children: [
-                  Image.network(
-                      'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
-                      width: 80,
-                      height: 100,
-                      fit: BoxFit.cover),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    '택시',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ],
+              MenuButton(
+                image_url:
+                    'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
+                sub_text: 'BMW',
               ),
-              Column(
-                children: [
-                  Image.network(
-                      'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
-                      width: 80,
-                      height: 100,
-                      fit: BoxFit.cover),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    '택시',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ],
+              MenuButton(
+                image_url:
+                    'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
+                sub_text: '벤츠',
               ),
-              Column(
-                children: [
-                  Image.network(
-                      'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
-                      width: 80,
-                      height: 100,
-                      fit: BoxFit.cover),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    '택시',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ],
+              MenuButton(
+                image_url:
+                    'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
+                sub_text: '아우디',
               ),
-              Column(
-                children: [
-                  Image.network(
-                      'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
-                      width: 80,
-                      height: 100,
-                      fit: BoxFit.cover),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    '택시',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ],
+              MenuButton(
+                image_url:
+                    'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
+                sub_text: '폭스바겐',
               ),
             ],
           ),
           SizedBox(
-            height: 30,
+            height: 16,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Column(
-                children: [
-                  Image.network(
-                      'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
-                      width: 80,
-                      height: 100,
-                      fit: BoxFit.cover),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    '택시',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ],
+              MenuButton(
+                image_url:
+                    'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
+                sub_text: '미니',
               ),
-              Column(
-                children: [
-                  Image.network(
-                      'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
-                      width: 80,
-                      height: 100,
-                      fit: BoxFit.cover),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    '택시',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ],
+              MenuButton(
+                image_url:
+                    'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
+                sub_text: '포르쉐',
               ),
-              Column(
-                children: [
-                  Image.network(
-                      'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
-                      width: 80,
-                      height: 100,
-                      fit: BoxFit.cover),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    '택시',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ],
+              MenuButton(
+                image_url:
+                    'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
+                sub_text: '부가티',
               ),
               SizedBox(
                 width: 80,
                 height: 80,
               ),
             ],
+          ),
+          SizedBox(
+            height: 16,
           ),
           SizedBox(
             height: 100,
@@ -165,19 +92,22 @@ class HomeScreen extends StatelessWidget {
                   title: '그러게 일찍 ',
                   body: '기왕 늦은거 편하게 택시타고 가',
                   color: Colors.red,
-                  image: 'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
+                  image:
+                      'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
                 ),
                 Ad(
                   title: '2번째 제목 ',
                   body: '기왕 늦은거 편하게 택시타고 가',
                   color: Colors.blue,
-                  image: 'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
+                  image:
+                      'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
                 ),
                 Ad(
                   title: '3번째 제목  ',
                   body: '기왕 늦은거 편하게 택시타고 가',
                   color: Colors.green,
-                  image: 'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
+                  image:
+                      'https://cdn.pixabay.com/photo/2021/09/08/15/24/couple-6607143_960_720.jpg',
                 ),
               ],
             ),
---
+++ lib/ui/home/widgets/menu_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MenuButton extends StatelessWidget {
+  final String image_url;
+  final String sub_text;
+
+  const MenuButton({Key key, this.image_url, this.sub_text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.network(
+            image_url,
+            width: 80,
+            height: 100,
+            fit: BoxFit.cover),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          sub_text,
+          style: TextStyle(fontSize: 12),
+        ),
+      ],
+    );
+  }
+}
