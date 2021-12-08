f6dca0abd93712ae05f761c8194e80bc7b97b778
iloneslife <jasony1@naver.com>
2021-12-08 12:00:02 +0900

    화면 이동시 title 전달

--- lib/ui/kakao_t/kakao_t_screen.dart
+++ lib/ui/kakao_t/kakao_t_screen.dart
@@ -3,6 +3,7 @@ import 'package:kakao_t_ui_exam/data/fake_data.dart';
 import 'package:kakao_t_ui_exam/model/ad.dart';
 import 'package:kakao_t_ui_exam/model/menu.dart';
 import 'package:kakao_t_ui_exam/ui/kakao_t//widgets/ad_view.dart';
+import 'package:kakao_t_ui_exam/ui/kakao_t/detail_screen.dart';
 import 'package:kakao_t_ui_exam/ui/kakao_t/widgets/menu_widget.dart';
 
 class KakaoTScreen extends StatelessWidget {
@@ -18,17 +19,17 @@ class KakaoTScreen extends StatelessWidget {
         ),
         backgroundColor: Colors.orange,
       ),
-      body: _buildBody(),
+      body: _buildBody(context),
     );
   }
 
-  Widget _buildBody() {
+  Widget _buildBody(BuildContext context) {
     PageController controller = PageController(initialPage: 0);
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: ListView(
         children: [
-          _buildMenu(),
+          _buildMenu(context),
           _buildAds(controller),
           _buildNotice(),
         ],
@@ -36,41 +37,57 @@ class KakaoTScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildMenu() {
+  Widget _buildMenu(BuildContext context) {
     return GridView.count(
       childAspectRatio: 1 / 1.3,
       physics: NeverScrollableScrollPhysics(),
       crossAxisCount: 4,
       shrinkWrap: true,
-      children: fakeMenus.map((Menu e) => MenuWidget(menu: e)).toList(), //Text(e.title)).toList(),
+      children: fakeMenus.map(
+        (Menu menu) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailScreen(
+                  title: menu.title,
+                )),
+              );
+            },
+            child: MenuWidget(menu: menu),
+          );
+        },
+      ).toList(), //Text(e.title)).toList(),
     );
   }
 
   Widget _buildAds(PageController controller) {
     return SizedBox(
-          height: 100,
-          child: PageView(
-            /// [PageView.scrollDirection] defaults to [Axis.horizontal].
-            /// Use [Axis.vertical] to scroll vertically.
-            scrollDirection: Axis.horizontal,
-            controller: controller,
-            children: fakeAds.map((Ad e) => AdView(ad: e)).toList(),
-          ),
-        );
+      height: 100,
+      child: PageView(
+        /// [PageView.scrollDirection] defaults to [Axis.horizontal].
+        /// Use [Axis.vertical] to scroll vertically.
+        scrollDirection: Axis.horizontal,
+        controller: controller,
+        children: fakeAds.map((Ad e) => AdView(ad: e)).toList(),
+      ),
+    );
   }
 
   Widget _buildNotice() {
     return Column(
-  //    shrinkWrap: true,  // 스크롤이 않되고, size 가 있는 column 처러 동작
-      children: List.generate(50, (index) => ListTile(
-        leading: Icon(
-          Icons.notifications_outlined,
-        ),
-        title: Text('공지 $index'),
-        trailing: Icon(
-          Icons.navigate_next_outlined,
-        ),
-      )),
+      //    shrinkWrap: true,  // 스크롤이 않되고, size 가 있는 column 처러 동작
+      children: List.generate(
+          50,
+          (index) => ListTile(
+                leading: Icon(
+                  Icons.notifications_outlined,
+                ),
+                title: Text('공지 $index'),
+                trailing: Icon(
+                  Icons.navigate_next_outlined,
+                ),
+              )),
     );
   }
 }
